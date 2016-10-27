3978af501e12403c2325dda50964cda1a1bc0c26
Ryan Carson <ryan@teamtreehouse.com>
2016-10-26 17:43:11 -0700

    Adding all the initial code

--- js/script.js
+++ js/script.js
@@ -2,4 +2,42 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// New comment is here
+// Global array to store our quotes
+var quotes = [
+  {
+    quote : "first quote",
+    source: "Bob jones",
+    citation: "http://www.abc.com",
+    year: 1977
+  },
+  {
+    quote : "second quote",
+    source: "Mother Teresa",
+    citation: "http://wikipedia.com",
+    year: 1981
+  },
+  {
+    quote : "third quote",
+    source: "Clinton",
+    citation: "http://bbc.co.uk",
+    year: 2016
+  }
+];
+
+// function to return a random quote
+function getRandomQuote (quote_array) {
+  return quote_array[Math.floor(Math.random() * quote_array.length)];
+}
+
+function printQuote () {
+  var selected_quote = getRandomQuote(quotes);
+  var html_string = '<p class="quote">' + selected_quote.quote + '</p> <p class="source">' + selected_quote.source;
+  if (selected_quote.citation) {
+    html_string += '<span class="citation">' + selected_quote.citation + '</span>';
+  }
+  if (selected_quote.year) {
+    html_string += '<span class="year">' + selected_quote.year + '</span>';
+  }
+  html_string +=  '</p>';
+  document.getElementById('quote-box').innerHTML = html_string;
+}
